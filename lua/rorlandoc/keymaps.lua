2158b47546029d1823d5a4d38bd07710c94433f8
Rui Costa <rorlandoc@gmail.com>
2024-10-23 23:49:22 +0100

    add git line blame

--- lua/rorlandoc/keymaps.lua
+++ lua/rorlandoc/keymaps.lua
@@ -3,3 +3,4 @@ vim.keymap.set("n", "<Esc>", "<cmd>nohlsearch<CR>", { desc = "Remove highlights"
 vim.keymap.set("t", "<Esc><Esc>", "<C-\\><C-n>", { desc = "Exit terminal mode" })
 
 vim.keymap.set("n", "<leader>gp", "<CMD>Gitsigns preview_hunk<CR>")
+vim.keymap.set("n", "<leader>gb", "<CMD>Gitsigns toggle_current_line_blame<CR>")
