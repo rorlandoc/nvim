28fbc449554724b9d67ec84b90ca0fcd2560f881
Rui Costa <rorlandoc@gmail.com>
2024-10-24 00:07:38 +0100

    added a couple of git keymaps

--- lua/rorlandoc/keymaps.lua
+++ lua/rorlandoc/keymaps.lua
@@ -2,5 +2,7 @@ vim.keymap.set("n", "<leader>e", vim.cmd.Ex, { desc = "Open file explorer" })
 vim.keymap.set("n", "<Esc>", "<cmd>nohlsearch<CR>", { desc = "Remove highlights" })
 vim.keymap.set("t", "<Esc><Esc>", "<C-\\><C-n>", { desc = "Exit terminal mode" })
 
+vim.keymap.set("n", "<leader>gs", vim.cmd.Git)
 vim.keymap.set("n", "<leader>gp", "<CMD>Gitsigns preview_hunk<CR>")
-vim.keymap.set("n", "<leader>gb", "<CMD>Gitsigns toggle_current_line_blame<CR>")
+vim.keymap.set("n", "<leader>gb", "<CMD>Git blame<CR>")
+vim.keymap.set("n", "<leader>gB", "<CMD>Gitsigns toggle_current_line_blame<CR>")
