febff0b771a1ee86d4c03696ecd4b9fefd716751
Rui Costa <rorlandoc@gmail.com>
2024-10-23 23:46:45 +0100

    add git hunk preview

--- lua/rorlandoc/keymaps.lua
+++ lua/rorlandoc/keymaps.lua
@@ -1,3 +1,5 @@
 vim.keymap.set("n", "<leader>e", vim.cmd.Ex, { desc = "Open file explorer" })
 vim.keymap.set("n", "<Esc>", "<cmd>nohlsearch<CR>", { desc = "Remove highlights" })
 vim.keymap.set("t", "<Esc><Esc>", "<C-\\><C-n>", { desc = "Exit terminal mode" })
+
+vim.keymap.set("n", "<leader>gp", "<CMD>Gitsigns preview_hunk<CR>")
