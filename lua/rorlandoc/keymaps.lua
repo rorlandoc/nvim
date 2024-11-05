bbe867d30d82940a100bc2a719a289814d92b9d6
Rui Costa <rorlandoc@gmail.com>
2024-11-05 21:32:22 +0000

    Added nvim tree

--- lua/rorlandoc/keymaps.lua
+++ lua/rorlandoc/keymaps.lua
@@ -1,4 +1,5 @@
-vim.keymap.set("n", "<leader>e", vim.cmd.Ex, { desc = "Open file explorer" })
+# vim.keymap.set("n", "<leader>e", vim.cmd.Ex, { desc = "Open file explorer" })
+vim.keymap.set("n", "<leader>e", vim.cmd.NvimTreeToggle)
 vim.keymap.set("n", "<Esc>", "<cmd>nohlsearch<CR>", { desc = "Remove highlights" })
 vim.keymap.set("t", "<Esc><Esc>", "<C-\\><C-n>", { desc = "Exit terminal mode" })
 
